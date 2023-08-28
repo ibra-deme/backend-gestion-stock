b3adbd79ebee9d9bf9d5cac5e1fd590f9c04229f
ibra Deme <diengdeguene87@gmail.com>
2023-08-28 17:40:09 +0200

    deploy

--- gestion_stock/settings.py
+++ gestion_stock/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4cyc8ht(ijpnlx15c21=(c9a7*61ekszu$sow0qnh8^^cv@sqy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
